625feb85b2b476c2adb380e3d85a912952d38c07
daoodaba975 <davinhoba9@outlook.fr>
2023-06-15 23:17:11 +0000

    🆕 :: add global variable.

---
+++ test/specs/global.js
@@ -0,0 +1,11 @@
+global.devtoXPath = {
+  searchInput: "//input[@placeholder='Search...']",
+  searchButton: "//button[@aria-label='Search']",
+  link: "//a[@id='article-link-949040']",
+};
+
+global.lienfyXPath = {
+  urlInput: "#id_url",
+  submitButton: "//button[@type='submit']",
+  copyButton: "//i[@class='bi bi-clipboard']",
+};
